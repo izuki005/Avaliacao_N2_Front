b3f5a41783bd2c335e3f801c919c57bc8d26cf22
kawan gabriel <kawan.regis@faculdadecesusc.edu.br>
2024-05-13 00:37:47 -0300

    Feito a troca do teste.html para funcionar via react

---
+++ n2-react-api/src/App.js
@@ -0,0 +1,14 @@
+
+// import './App.css';
+import MeuComponente from './index.jsx';
+
+function App() {
+  return (
+    <div className="App">
+        <MeuComponente></MeuComponente>
+    </div>
+  );
+}
+
+
+export default App;
